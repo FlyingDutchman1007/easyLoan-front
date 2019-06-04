e77ef80f5564b62429ef451ed2abf60f41630706
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-04 19:11:00 +0800

    修改跳转地址

--- web_qsd/static/js/borrower_deposit.js
+++ web_qsd/static/js/borrower_deposit.js
@@ -11,16 +11,16 @@ $("#borrower_deposit").click(function () {
             alert(message.state);
             if(message.state == "successful") {
                 alert("充值成功");
-                location.href = "/resources/lender/fund_account";
+                location.href = "/resources/borrower/fund_account";
             }
             else{
                 alert("充值失败，请重新充值~");
-                location.href = "/resources/lender/fund_account";
+                location.href = "/resources/borrower/fund_account";
             }
         },
         error: function () {
             alert("充值失败，请重新充值~");
-            location.href = "/resources/lender/fund_account";
+            location.href = "/resources/borrower/fund_account";
         }
     })
 });
