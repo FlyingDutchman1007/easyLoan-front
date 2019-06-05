19e95bf947060f921e2e67cf516d3eaffce2a2e0
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-05 19:38:30 +0800

    11~14行增加字段

--- web_qsd/static/assets/js/fund_account_lr.js
+++ web_qsd/static/assets/js/fund_account_lr.js
@@ -8,6 +8,10 @@ $(document).ready(
             $("#user_name").text(res.user_name);
             $("#bank_account").text(res.bank_account);
             $("#account_balance").text(res.user_balance);
+            $("#current_income").text(res.current_income);
+            $("#expected_income").text(res.expected_income);
+			$("#lend_money").text(res.lend_money);
+            $("#available_money").text(res.available);
         },
         error: function() {}
 
