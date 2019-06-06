3d105772a9d91585d2a538ded13b45c92d191617
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-06 14:37:31 +0800

    8~14修改text 方法 为 val 方法

--- web_qsd/static/assets/js/fund_account_lr.js
+++ web_qsd/static/assets/js/fund_account_lr.js
@@ -5,13 +5,13 @@ $(document).ready(
         dataType: "json",
         url: "/trade/lender/account",
         success: function (res) {
-            $("#user_name").text(res.user_name);
-            $("#bank_account").text(res.bank_account);
-            $("#account_balance").text(res.user_balance);
-            $("#current_income").text(res.current_income);
-            $("#expected_income").text(res.expected_income);
-	    $("#lend_money").text(res.lend_money);
-            $("#available_money").text(res.available_money);
+            $("#user_name").val(res.user_name);
+            $("#bank_account").val(res.bank_account);
+            $("#account_balance").val(res.user_balance);
+            $("#current_income").val(res.current_income);
+            $("#expected_income").val(res.expected_income);
+	    $("#lend_money").val(res.lend_money);
+            $("#available_money").val(res.available_money);
         },
         error: function() {}
 
