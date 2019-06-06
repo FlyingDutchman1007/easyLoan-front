0959b8e861c48bc090b48c57fffe8ee3ffb6b98c
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-06 09:46:22 +0800

    提交借入方待还款js文件

--- web_qsd/static/assets/js/repayment_record.js
+++ web_qsd/static/assets/js/repayment_record.js
@@ -1,46 +1,57 @@
-$("#repayment_record").DateTable ({
-	"autoWidth": false,
-	"serverSide": true,
-	"ajax": {
-		url: "/trade/borrower/loan",
-		method: "POST",
-		data: {
-			"bill_id": $('#bill_id').val(),
-			"start_date": $('#start_date').val(),
-			"start_money": $('#start_money').val(),
-			"unpay_money": $('#unpay_money').val(),
-			"next_time_should_pay": $('#next_time_should_pay').val(),
-			"liquidated_money": $('#liquidated_money').val(),
-			"pay_rate": $('#pay_rate').val(),
-			"pay_type": $('#pay_type').val(),
-			"deadline": $('#deadline').val(),
-			"start_interest": $('#start_interest').val()
-		
-			},
-	},
-		"columnDefs": [
-		       {
-			   "targets": 0,
-			   "visible": false,
-				},
-		       {
-			   targets: 11,
-			   render: function (data, type,row) {
-					   return '<button type="button" class="btn btn-info">还款</button>';
-			   }
-			  }			   
-			],  
-	"columns": [//返回的json数据在这里填充，注意一定要与上面的<th>数量对应，否则排版出现扭曲
-	    { "data": "bill_id"},
-	    { "data": "start_date" },
-	    { "data": "start_interest" },
-	    { "data": "start_money" },
-	    { "data": "pay_rate" },
-	    { "data": "pay_type" },
-	    { "data": "next_time_should_pay" },
-	    { "data": "unpay_money" },
-		{ "data": "deadline" },
-		{ "data": "liquidated_money" },
-		{ "data": null }
-		]
+$(function(){
+    $.ajax({
+        type: "get",
+        url: "/trade/borrower/loan",
+        dataType: "json",
+        success: function (res) {
+            var list = res;
+            for($i = 0;$i<list.length;$i++) {
+                var temp = "";
+                var type = "月";
+                if(list[$i].pay_type === "3"){
+                    var type = "季";
+                }
+                temp +=
+                    '<tr>'+
+                    '<td>' + list[$i].bill_id + '</td>' +
+                    '<td>' + list[$i].start_date + '</td>' +
+                    '<td>' + list[$i].start_money + "元" + '</td>' +
+                    '<td>' + list[$i].unpay_money + "元" + '</td>' +
+                    '<td>' + list[$i].next_time_should_pay + "元" + '</td>' +
+                    '<td>' + list[$i].liquidated_money + "元" + '</td>' +
+                    '<td>' + list[$i].pay_rate + "%" + '</td>' +
+                    '<td>' + type + '</td>' +
+                    '<td>' + list[$i].deadline  + '</td>' +
+                    '<td>' + list[$i].start_interest + '</td>' +
+                    '<td>' + '<button type="button" class="btn btn-success" id="repay">还款</button>' + '</td>' +
+                    '</tr>';
+                $("#tbody").append(temp);
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $("#repay").click(function () {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                "bill_id": $("table tr:eq(0) td:eq(0)").text(),
+                "money": $("table tr:eq(0) td:eq(4)").text(),
+                "exact_date": $(Date().toLocaleTimeString()),
+            }),
+            success: function (message) {
+                if (message.state === "successful" ) {
+                    alert("还款成功！");
+                }
+            },
+            error: function () {
+                alert("还款失败，请重新还款！");
+            }
+        })
+    })
 });
