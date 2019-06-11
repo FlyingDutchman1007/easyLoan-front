a4ba06da209d500268847ae8d7b9c35c029adfa1
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-11 09:19:23 +0800

    重构login.js，添加判断方法

--- web_qsd/static/js/login.js
+++ web_qsd/static/js/login.js
@@ -1,32 +1,58 @@
 	    $("#login_btn").click(function () {
-        $.ajax({
-        type: "POST",
-        url: "/identity/all/login",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({
-            "phone_number": $("#phone_number").val(),
-            "password": $("#password").val()
-        }),
-        dataType: "json",
-        success: function (message) {
-			alert(message.state);
-            if (message.state != -1 ) {
-				if(message.state == 0){
-					alert("借入者登录成功！");
-					location.href = "/resources/borrower/index_borrower";
-				}else{
-					alert("借出者登录成功！");
-					location.href = "/resources/lender/index_lender";
-				}
-            }
-			else{
-				alert("登录失败，请重新登录~");
-				location.href = "/static/login.html";
-			}
-        },
-        error: function () {
-            alert("登录失败，请重新登录~");
-			location.href = "/static/login.html";
-        }
-		})
+            var phone_number = $("#phone_number").val();
+            var res = false;
+	       if(phone_number.length ==11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(phone_number)) {
+               $.ajax({
+                   type: "POST",
+                   url: "/information/all/checkPhoneNumber",
+                   contentType: "application/json; charset=utf-8",
+                   async: false,
+                   data: JSON.stringify({
+                       "phone_number": phone_number
+                   }),
+                   dataType: "json",
+                   success: function (message) {
+                       if (message.state == "unregister") {
+                           res = false;
+                       } else {
+                           res = true;
+                       }
+                   },
+                   error: function () {
+                       res = false;
+                   }
+               });
+               if (res == true) {
+                   $.ajax({
+                       type: "POST",
+                       url: "/identity/all/login",
+                       contentType: "application/json; charset=utf-8",
+                       data: JSON.stringify({
+                           "phone_number": $("#phone_number").val(),
+                           "password": $("#password").val()
+                       }),
+                       dataType: "json",
+                       success: function (message) {
+                           alert(message.state);
+                           if (message.state != -1) {
+                               if (message.state == 0) {
+                                   alert("借入者登录成功！");
+                                   location.href = "../../resources/borrower/index_borrower.html";
+                               } else {
+                                   alert("借出者登录成功！");
+                                   location.href = "../../resources/lender/index_lender.html";
+                               }
+                           }
+                           else {
+                               alert("登录失败，请重新登录~");
+                               location.href = "login.html";
+                           }
+                       },
+                       error: function () {
+                           alert("登录失败，请重新登录~");
+                           location.href = "login.html";
+                       }
+                   })
+               }
+           }
     });
