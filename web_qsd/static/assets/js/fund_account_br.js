3b65c20b664a3674be85c8137b6f44a97df15e07
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-12 16:05:20 +0800

    修改字段命名

--- web_qsd/static/assets/js/fund_account_br.js
+++ web_qsd/static/assets/js/fund_account_br.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
         dataType: "json",
         url: "/trade/borrower/account",
         success: function (res) {
-            $("#user_name").text(res.user_name);
-            $("#credit_score").text(res.credit_score);
-            $("#bank_account").text(res.bank_account);
-            $("#account_balance").text(res.account_balance);
-            $("#total_limit").text(res.total_limit);
-            $("#available_limit").text(res.available_limit);
+            $("#user_name").text(res.userName);
+            $("#credit_score").text(res.creditScore);
+            $("#bank_account").text(res.bankAccount);
+            $("#account_balance").text(res.accountBalance);
+            $("#total_limit").text(res.totalLimit);
+            $("#available_limit").text(res.availableLimit);
         },
         error: function() {}
     })
