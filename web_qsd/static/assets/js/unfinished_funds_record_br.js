f83e556dc034175f0930d3ef5eb506a496cab01e
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-12 19:18:58 +0800

    修改字段命名

--- web_qsd/static/assets/js/unfinished_funds_record_br.js
+++ web_qsd/static/assets/js/unfinished_funds_record_br.js
@@ -1,7 +1,7 @@
 $(function(){
    $.ajax({
        type: "get",
-       url: "ufrd.json",
+       url: "br_unfin.json",
        dataType: "json",
        success: function (res) {
            var list = res;
@@ -9,18 +9,18 @@ $(function(){
            var money = 0;
            for (ls in list) {
                //temp存放表单信息
-			   temp += '<td>' + list[ls].bill_id + '</td>';
-			   temp += '<td>' + list[ls].intend_money + '</td>';
-			   temp += '<td>' + list[ls].start_date + '</td>';
+			   temp += '<td>' + list[ls].billId + '</td>';
+			   temp += '<td>' + list[ls].intendMoney + '</td>';
+			   temp += '<td>' + list[ls].startDate + '</td>';
 			   temp += '<td>' + list[ls].rate + '</td>';
-			   if (list[ls].pay_type == 1){
+			   if (list[ls].payType == 1){
                     temp += '<td>' + '按月还' + '</td>';
                        } else{
 					temp += '<td>' + '按季还' + '</td>';
                        }
-				temp += '<td>' + list[ls].limit_months + '</td>';
-				temp += '<td>' + list[ls].raised_money + '</td>';				
-				money = list[ls].intend_money - list[ls].raised_money
+				temp += '<td>' + list[ls].limitMonths + '</td>';
+				temp += '<td>' + list[ls].raisedMoney + '</td>';
+				money = list[ls].intendMoney - list[ls].raisedMoney
            }
            $("#unfinished").html(temp);
            $("#money").text(money + "元");
