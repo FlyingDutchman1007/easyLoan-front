14f04764a966e5bcf4b637a8b0806e5386e428bc
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-12 19:49:02 +0800

    修改url和跳转

--- web_qsd/static/js/borrower_withdraw.js
+++ web_qsd/static/js/borrower_withdraw.js
@@ -1,7 +1,7 @@
 $("#borrower_withdraw").click(function () {
     $.ajax({
         type: "POST",
-        url: "/trade/general/withdraw",
+        url: "http://192.168.0.195:8080/subWithdraw",
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify({
             "money": $("#b_withdraw_money").val()
@@ -11,17 +11,16 @@ $("#borrower_withdraw").click(function () {
             alert(message.state);
             if(message.state == "successful") {
                 alert("提现成功");
-                location.href = "/resources/borrower/fund_account";
+                location.href = "fund_account.html";
             }
             else{
                 alert("提现失败，请重新操作~");
-                location.href = "/resources/borrower/fund_account";
+                location.href = "fund_account.html";
             }
-
         },
         error: function () {
             alert("发送失败，请稍后再试~");
-            location.href = "/resources/borrower/fund_account";
+            location.href = "fund_account.html";
         }
     })
 });
