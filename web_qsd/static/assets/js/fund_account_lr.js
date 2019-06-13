4e76ec6c5d91e152e9369b5719ab6241212e5fcc
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-13 15:51:30 +0800

    修改字段名称

--- web_qsd/static/assets/js/fund_account_lr.js
+++ web_qsd/static/assets/js/fund_account_lr.js
@@ -8,9 +8,9 @@ $(document).ready(
             $("#user_name").text(res.user_name);
             $("#bank_account").text(res.bank_account);
             $("#account_balance").text(res.account_balance);
-            $("#current_income").text(res.current_income);
-            $("#expected_income").text(res.expected_income);
-	    $("#lend_money").text(res.lent_money);
+            $("#current_income").text(res.current_benefit);
+            $("#expected_income").text(res.expected_benefit);
+	    $("#lend_money").text(res.lend_money);
             $("#available_money").text(res.available_money);
         },
         error: function() {}
