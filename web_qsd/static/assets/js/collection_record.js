975170ad1e670424010e9b3fb53679f7dce58c0a
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-17 16:09:01 +0800

    重构collection_record.js

--- web_qsd/static/assets/js/collection_record.js
+++ web_qsd/static/assets/js/collection_record.js
@@ -1,44 +1,36 @@
-$(#repayment_record).DateTable ({
-	"autoWidth": false,
-	"serverSide": true,
-	"ajax": {
-		url: "/trade/lender/loan"
-		method: "POST",
-		data: {
-			"bill_id": $('#bill_id').val(),
-			"start_date": $('#start_date').val(),
-			"start_money": $('#start_money').val(),
-			"unpay_money": $('#unpay_money').val(),
-			"next_time_should_pay": $('#next_time_should_pay').val(),
-			"liquidated_money": $('#liquidated_money').val(),
-			"pay_rate": $('#pay_rate').val(),
-			"pay_type": $('#pay_type').val(),
-			"deadline": $('#deadline').val(),
-			"start_interest": $('#start_interest').val()
-		      },
-			},
-		"columnDefs": [
-		       {
-			   "targets": 0,
-			   "visible": false,
-				},
-		       {
-			   targets: 11,
-			   render: function (data, type,row) {
-					   return '<button type="button" class="btn btn-info">还款</button>';
-			   } 
-			],  
-	"columns": [//返回的json数据在这里填充，注意一定要与上面的<th>数量对应，否则排版出现扭曲
-	    { "data": "bill_id"},
-	    { "data": "start_date" },
-	    { "data": "start_interest" },
-	    { "data": "start_money" },
-	    { "data": "pay_rate" },
-	    { "data": "pay_type" },
-	    { "data": "next_time_should_pay" },
-	    { "data": "unpay_money" },
-		{ "data": "deadline" },
-		{ "data": "liquidated_money" }，
-		{ "data": null }
-	]
+$(document).ready(function () {
+    $.ajax({
+        type : "get",
+        dataType : "json",
+        url : "http://192.168.0.195:8080/lenderToReceive",
+        xhrFields:{
+            withCredentials:true
+        },
+        success : function (list) {
+            for($i = 0;$i<list.length;$i++){
+                var temp = "";
+                var type = "按月付";
+                if(list[$i].payType == "3"){
+                    type = "按季付";
+                }
+                temp +=
+                    '<tr>'+
+                    '<td>' + list[$i].billId + '</td>' +
+                    '<td>' + list[$i].money + "元" + '</td>' +
+                    '<td>' + list[$i].collectedMoney + "元" + '</td>' +
+                    '<td>' + list[$i].interest + "元" + '</td>' +
+                    '<td>' + list[$i].collectedInterest + "元" + '</td>' +
+                    '<td>' + list[$i].liquidatedMoney + "元" + '</td>' +
+                    '<td>' + list[$i].collectedLiquidatedMoney + "元" + '</td>' +
+                    '<td>' + list[$i].totalMoney + "元" + '</td>' +
+                    '<td>' + list[$i].collectedTotalMoney + "元" + '</td>' +
+                    '<td>' + list[$i].payRate + "%" + '</td>' +
+                    '<td>' + type + '</td>' +
+                    '<td>' + list[$i].startDate + '</td>' +
+                    '<td>' + list[$i].deadline + '</td>' +
+                    '</tr>';
+                $("#data").append(temp);
+            }
+        }
+    });
 });
