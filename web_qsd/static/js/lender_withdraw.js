17f3ac8ab90ba5f068ebdf91ff7d5c55efea9e18
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-17 19:31:57 +0800

    修改文字提示和url

--- web_qsd/static/js/lender_withdraw.js
+++ web_qsd/static/js/lender_withdraw.js
@@ -1,7 +1,10 @@
 $("#lender_withdraw").click(function () {
     $.ajax({
         type: "POST",
-        url: "/trade/general/withdraw",
+        url: "http://192.168.0.195:8080/lenderWithdraw",
+        xhrFields:{
+            withCredentials:true
+        },
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify({
             "money": $("#l_withdraw_money").val()
@@ -11,17 +14,17 @@ $("#lender_withdraw").click(function () {
             alert(message.state);
                 if(message.state == "successful") {
                     alert("提现成功");
-                    location.href = "/resources/lender/fund_account";
+                    location.href = "fund_account.html";
                 }
                 else{
-                    alert("提现失败，请重新提现~");
-                    location.href = "/resources/lender/fund_account";
+                    alert("提现失败，请稍后再试~");
+                    location.href = "fund_account.html";
                 }
 
         },
         error: function () {
-            alert("发送失败，请稍后再试~");
-            location.href = "/resources/lender/fund_account";
+            alert("提交失败，请稍后再试~");
+            location.href = "fund_account.html";
         }
     })
 });
