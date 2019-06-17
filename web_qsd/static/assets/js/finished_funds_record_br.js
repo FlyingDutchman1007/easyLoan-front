6303c3ce605e9ca868fcec7d8b19dc718e278b7c
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-17 10:08:36 +0800

    重构finished_funds_record_br.js

--- web_qsd/static/assets/js/finished_funds_record_br.js
+++ web_qsd/static/assets/js/finished_funds_record_br.js
@@ -2,58 +2,29 @@ $(function(){
    $.ajax({
        type : "get",
        dataType : "json",
-       url : "/trade/borrower/finishedLoan",
-       success : function (res) {
-
-           var list = res ;
+       url : "fin_rec.json",
+       success : function (list) {
            for($i = 0;$i<list.length;$i++){
-
                var temp = "";
-               var type = "月";
-               var all_money = 0;
-               console.log($i);
-               /*
-               list[$i].bill_id = ;
-               list[$i].start_date = ;
-               list[$i].money = ;
-               list[$i].interest = ;
-               list[$i].rate = ;
-               list[$i].pay_type = ;
-               list[$i].limit_months = ;
-               list[$i].settle_date = ;
-                */
+               var type = "按月付";
                if(list[$i].pay_type == "3"){
-                   var type = "季";
+                   type = "按季付";
                }
-               all_money = parseInt(list[$i].money) + parseInt(list[$i].interest);
-
                temp +=
                    '<tr>'+
-                   '<td>' + list[$i].bill_id + '</td>' +
-                   '<td>' + list[$i].start_date + '</td>' +
+                   '<td>' + list[$i].billId + '</td>' +
                    '<td>' + list[$i].money + "元" + '</td>' +
                    '<td>' + list[$i].interest + "元" + '</td>' +
+                   '<td>' + list[$i].liquidatedMoney + "元" + '</td>' +
+                   '<td>' + list[$i].totalMoney + "元" + '</td>' +
                    '<td>' + list[$i].rate + "%" + '</td>' +
                    '<td>' + type + '</td>' +
-                   '<td>' + list[$i].limit_months + "月" + '</td>' +
-                   '<td>' + list[$i].settle_date + '</td>' +
-                   '<td class="text-success">' + all_money + "元" + '</td>' +
+                   '<td>' + list[$i].limitMonths + "月" + '</td>' +
+                   '<td>' + list[$i].startDate + '</td>' +
+                   '<td>' + list[$i].settleDate + '</td>' +
                    '</tr>';
-
                $("#tbody").append(temp);
-
            }
        }
    });
 });
-
-/*
-"bill_id" : "001" ,
-    "start_date" : "2018-07-06",
-    "money" : "1000",
-    "interest" : "100",
-    "rate" : "1",
-    "pay_type" : "1",
-    "limit_nomths" : "5",
-    "settle_date" : "2018-12-06"
- */
