69188d8e9aaa821585a95812ff289280902e4146
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-18 15:07:40 +0800

    重构

--- web_qsd/static/js/mate_lend.js
+++ web_qsd/static/js/mate_lend.js
@@ -1,20 +1,27 @@
 $(function() {
+    var url = decodeURI(window.location.href);
+    var argsIndex = url .split("?");
+    var money = argsIndex[1].split("&&")[0].split("=")[1];
+    var pay_type = argsIndex[1].split("&&")[1].split("=")[1];
+    var rate = argsIndex[1].split("&&")[2].split("=")[1];
+    var limit_months = argsIndex[1].split("&&")[3].split("=")[1];
     $.ajax({
-        type: "get",
-        url: "/trade/lender/searchBorrowers",
+        type: "POST",
+        url: "http://192.168.0.195:8080/lendMatch",
+        xhrFields:{
+            withCredentials:true
+        },
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            "expectRate": rate,
+            "lendMoney": money,
+            "payType": pay_type,
+            "limitMonths": limit_months
+        }),
         dataType: "json",
-        success: function (res) {
-            var list = res;
-            /* }
-          })
-      });*/
-
-            //console.log(list[0].intend_money);
+        success: function (list) {
             for (var $i = 0; $i < list.length; $i++) {
-                //console.log($i);
-                //temp存放表单信息
                 var temp = "";
-                //if ($i == 0) {
                 temp +=
                     '<li>' + "借款金额:" + '<strong>' + list[$i].intend_money + '</strong>' + "元" + '</li>' +
                     '<li>' + "起始日期:" + '<strong>' + list[$i].start_date + '</strong>'  + '</li>' +
@@ -22,19 +29,10 @@ $(function() {
                     '<li>' + "还款方式:" + '<strong>' + list[$i].pay_type + '</strong>'  + '</li>' +
                     '<li>' + "期限&nbsp&nbsp:" + '<strong>' + list[$i].limit_momths + '</strong>' + "月" + '</li>' +
                     '<li>' + "已筹集金额:" + '<strong>' + list[$i].raised_money + '</strong>' + "元" + '</li>';
-                //console.log(temp);
                 $("#list_info" + ($i+1)).html(temp);
-                //计算还需要的金额
                 $("#surplus" + ($i+1)).text(parseInt(list[$i].intend_money) - parseInt(list[$i].raised_money));
-                //$("#list_info1").text("aaa</li>");
                 console.log(temp);
             }
-
-
-            //$("#my_money").text(my_money());
-            //$("#my_rate").text(my_rate());
-            //$("#my_month").text(my_month());
-
         }
     })
 });
