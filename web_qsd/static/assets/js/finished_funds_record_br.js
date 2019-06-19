ad0369d7233dcbeef05c46c3e4325bc083233e06
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-19 17:04:02 +0800

    修改url

--- web_qsd/static/assets/js/finished_funds_record_br.js
+++ web_qsd/static/assets/js/finished_funds_record_br.js
@@ -2,7 +2,7 @@ $(function(){
    $.ajax({
        type : "get",
        dataType : "json",
-       url : "http://192.168.0.195:8080/borrowerFinishedRecord",
+       url : "http://127.0.0.1:8080/borrowerFinishedRecord",
       xhrFields:{
             withCredentials:true
         },
