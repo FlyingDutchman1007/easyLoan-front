209f35f05eeae4ca1657fe5cfc861d0ad3c063db
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-19 17:05:13 +0800

    修改url

--- web_qsd/static/assets/js/fund_account_lr.js
+++ web_qsd/static/assets/js/fund_account_lr.js
@@ -3,7 +3,7 @@ $(document).ready(
     $.ajax({
         type: "get",
         dataType: "json",
-        url: "http://192.168.0.195:8080/lenderFund",
+        url: "http://127.0.0.1:8080/lenderFund",
         xhrFields:{
             withCredentials:true
         },
