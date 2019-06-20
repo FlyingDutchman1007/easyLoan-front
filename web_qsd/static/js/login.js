b273e9b55380d2fb0b71fd18a452d1aaa08f2152
Iwannabeblownaway <44230160+Iwannabeblownaway@users.noreply.github.com>
2019-06-20 11:01:47 +0800

    删除alert

--- web_qsd/static/js/login.js
+++ web_qsd/static/js/login.js
@@ -42,13 +42,10 @@
                        }),
                        dataType: "json",
                        success: function (message) {
-                           alert(message.state);
                            if (message.state != -1) {
                                if (message.state == 0) {
-                                   alert("借入者登录成功！");
                                    location.href = "../resources/borrower/index_borrower.html";
                                } else {
-                                   alert("借出者登录成功！");
                                    location.href = "../resources/lender/index_lender.html";
                                }
                            }
